ced3cd8e3ba5d1e1d631c2834c1defd8453ef2ab
Brian Barto <bartobrian@gmail.com>
2021-12-31 16:07:41 -0500

    Remove log and test statements
    	modified:   brian.py

--- brian.py
+++ brian.py
@@ -15,7 +15,6 @@ def brian(plugin):
 	""" Function Descr Here """
 
 	reply = {}
-	## needs_balance = {}
 	needs_drain = {}
 	needs_fill = {}
 
@@ -37,10 +36,6 @@ def brian(plugin):
 				elif (ours <= low_thresh):
 					needs_fill[scid] = int(mid - ours)
 
-	## Just for testing. REMOVE ME.
-	needs_fill['715322x2292x1'] = needs_drain['715322x2292x1']
-	del needs_drain['715322x2292x1']
-
 	for scid_f in needs_fill:
 		for scid_d in needs_drain:
 
@@ -49,8 +44,6 @@ def brian(plugin):
 
 			if (needs_drain[scid_d] == 0):
 				continue
-
-			plugin.log(f"Proc {scid_d} = {needs_drain[scid_d]} and {scid_f} = {needs_fill[scid_f]}")
 
 			if (needs_fill[scid_f] > needs_drain[scid_d]):
 				amount = needs_drain[scid_d]
